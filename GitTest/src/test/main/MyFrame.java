3c0c2578d80f745a881bbe28866e383a912a61b6
Leejungmin <jml4254@gmail.com>
2019-12-27 16:06:04 +0900

    전송버튼 기능구현됨

--- GitTest/src/test/main/MyFrame.java
+++ GitTest/src/test/main/MyFrame.java
@@ -1,19 +1,39 @@
 package test.main;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class MyFrame extends JFrame{
+public class MyFrame extends JFrame implements ActionListener{
 	//생성자
 	public MyFrame(String title) {
 		super(title);
 		setBounds(100,100,300,400); //프레임 x좌표 y좌표 width,height 
 		setLayout(new BorderLayout());//프레임의 레이아웃 법칙 지정하기
 		setDefaultCloseOperation(EXIT_ON_CLOSE); //프레임 닫았을때 process 종료되도록
-		setVisible(true);
+		
+		
+		JButton sendBtn = new JButton("전송"); //버튼 만들기
+		
+		sendBtn.setActionCommand("send"); //버튼눌렀을때 동작할 command 지정하기
+		add(sendBtn,BorderLayout.NORTH); //버튼을 프레임의 북쪽에 배치하기
+		sendBtn.addActionListener(this); //버튼에 액션리스너 등록하기
+		setVisible(true);//프레임이 화면에 보이도록 설정
 	}
 	public static void main(String[] args) {
 		new MyFrame("나의프레임");
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//눌러진 버튼의 액션 command를 읽어온다.
+		String command=e.getActionCommand();
+		//액션 command 가 무엇이냐에 따라서 분기하기
+		if(command.equals("send")) {
+			JOptionPane.showMessageDialog(this, "전송합니다.");
+		}
 	}
 }
